5505a0d005e015828ce877d11582ad72ac4cc763
David Tanzer <david@davidtanzer.net>
2024-12-19 11:37:04 +0100

    Tables pass the legacy MfM tests

--- test/legacy/parseMarkdown_Tables.spec.ts
+++ test/legacy/parseMarkdown_Tables.spec.ts
@@ -14,26 +14,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { parseMarkdown, Table } from '../../src/legacy/parseMarkdown'
+import { Table } from '../../src/element'
+import { parseMarkdown } from '../../src/legacy/parseMarkdown'
 import {assertTextContent} from './assertion.textContent'
 
 const assume = expect
 
-describe.skip('parseMarkdown: Tables', () => {
+describe('parseMarkdown: Tables', () => {
 	it('parses empty table as table', () => {
 		const markdown = '| abc | def |\n|-|- |'
 
 		const result = parseMarkdown(markdown)
 
 		expect(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.headings).toHaveLength(2)
-		expect(table.headings[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[0].content[0]).toHaveProperty('content', 'abc')
+		expect(table.headers?.columns).toHaveLength(2)
+		expect(table.headers?.columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[0].content[0]).toHaveProperty('textContent', ' abc ')
 
-		expect(table.headings[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[1].content[0]).toHaveProperty('content', 'def')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('textContent', ' def ')
 	})
 
 	it('parses table with two rows', () => {
@@ -42,20 +43,20 @@ describe.skip('parseMarkdown: Tables', () => {
 		const result = parseMarkdown(markdown)
 
 		expect(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.rows).toHaveLength(2)
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('content', '1,1')
+		expect(table.tableRows).toHaveLength(2)
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('textContent', '1,1')
 
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('content', '1,2')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('textContent', ' 1,2    ')
 
-		expect(table.rows[1].columns[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[1].columns[0].content[0]).toHaveProperty('content', '2,1')
+		expect(table.tableRows[1].columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[1].columns[0].content[0]).toHaveProperty('textContent', '2,1')
 
-		expect(table.rows[1].columns[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[1].columns[1].content[0]).toHaveProperty('content', '2,2')
+		expect(table.tableRows[1].columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[1].columns[1].content[0]).toHaveProperty('textContent', '2,2')
 	})
 
 	it('parses table with no headers', () => {
@@ -64,32 +65,32 @@ describe.skip('parseMarkdown: Tables', () => {
 		const result = parseMarkdown(markdown)
 
 		expect(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.headings).toHaveLength(0)
+		expect(table.headers).toBeNull()
 
-		expect(table.rows).toHaveLength(1)
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('content', '1,1')
+		expect(table.tableRows).toHaveLength(1)
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('textContent', '1,1')
 
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('content', '1,2')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('textContent', ' 1,2    ')
 	})
 
 	it('parses table options after first row', () => {
-		const markdown = '| abc | def |{chart;type=bars}\n|-|- |'
+		const markdown = '| abc | def |\n|-|- |{chart;type=bars}'
 
 		const result = parseMarkdown(markdown)
 
 		assume(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.headings).toHaveLength(2)
-		expect(table.headings[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[1].content[0]).toHaveProperty('content', 'def')
+		expect(table.headers?.columns).toHaveLength(2)
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('textContent', ' def ')
 
-		expect(table.options).toHaveProperty('default', 'chart')
-		expect(table.options).toHaveProperty('type', 'bars')
+		expect(table.options.get('default')).toEqual('chart')
+		expect(table.options.get('type')).toEqual('bars')
 	})
 
 	it('parses table with empty column', () => {
@@ -98,20 +99,20 @@ describe.skip('parseMarkdown: Tables', () => {
 		const result = parseMarkdown(markdown)
 
 		expect(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.headings).toHaveLength(0)
+		expect(table.headers).toBeNull()
 
-		expect(table.rows).toHaveLength(1)
-		expect(table.rows[0].columns).toHaveLength(3)
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('content', '1,1')
+		expect(table.tableRows).toHaveLength(1)
+		expect(table.tableRows[0].columns).toHaveLength(3)
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('textContent', '1,1')
 
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[1].content[0]).toHaveProperty('content', '')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[1].content[0]).toHaveProperty('textContent', '   ')
 
-		expect(table.rows[0].columns[2].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[2].content[0]).toHaveProperty('content', '1,3')
+		expect(table.tableRows[0].columns[2].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[2].content[0]).toHaveProperty('textContent', '1,3')
 	})
 
 	it('parses table and keeps last column even without last pipe', () => {
@@ -120,35 +121,35 @@ describe.skip('parseMarkdown: Tables', () => {
 		const result = parseMarkdown(markdown)
 
 		expect(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as  Table
 
-		expect(table.headings).toHaveLength(0)
+		expect(table.headers).toBeNull()
 
-		expect(table.rows).toHaveLength(1)
-		expect(table.rows[0].columns).toHaveLength(3)
-		expect(table.rows[0].columns[2].content[0]).toHaveProperty('type', 'Text')
-		expect(table.rows[0].columns[2].content[0]).toHaveProperty('content', '1,3')
+		expect(table.tableRows).toHaveLength(1)
+		expect(table.tableRows[0].columns).toHaveLength(3)
+		expect(table.tableRows[0].columns[2].content[0]).toHaveProperty('type', 'Text')
+		expect(table.tableRows[0].columns[2].content[0]).toHaveProperty('textContent', '1,3')
 	})
 
 	it('parses paragraph content in table header', () => {
-		const markdown = '| **abc** | def `ghi` |\n|-|- |\n|1,1| 1,2    |\n|2,1|2,2|'
+		const markdown = '| **abc** | def _ghi_ |\n|-|- |\n|1,1| 1,2    |\n|2,1|2,2|'
 
 		const result = parseMarkdown(markdown)
 
 		assume(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		assume(table.headings).toHaveLength(2)
+		assume(table.headers?.columns).toHaveLength(2)
 
-		expect(table.headings[0].content).toHaveLength(1)
-		expect(table.headings[0].content[0]).toHaveProperty('type', 'Bold')
-		assertTextContent(table.headings[0].content[0], 'abc')
+		expect(table.headers?.columns[0].content).toHaveLength(3)
+		expect(table.headers?.columns[0].content[1]).toHaveProperty('type', 'StrongEmphasis')
+		assertTextContent(table.headers?.columns[0].content[1], 'abc')
 
-		expect(table.headings[1].content).toHaveLength(2)
-		expect(table.headings[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[1].content[0]).toHaveProperty('content', 'def ')
+		expect(table.headers?.columns[1].content).toHaveLength(3)
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('textContent', ' def ')
 
-		expect(table.headings[1].content[1]).toHaveProperty('type', 'InlineCode')
+		expect(table.headers?.columns[1].content[1]).toHaveProperty('type', 'Emphasis')
 	})
 
 	it('parses paragraph content in table content', () => {
@@ -157,11 +158,11 @@ describe.skip('parseMarkdown: Tables', () => {
 		const result = parseMarkdown(markdown)
 
 		assume(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		assume(table.rows).toHaveLength(2)
-		expect(table.rows[0].columns[0].content[0]).toHaveProperty('type', 'Bold')
-		assertTextContent(table.rows[0].columns[0].content[0], '1,1')
+		assume(table.tableRows).toHaveLength(2)
+		expect(table.tableRows[0].columns[0].content[0]).toHaveProperty('type', 'StrongEmphasis')
+		assertTextContent(table.tableRows[0].columns[0].content[0], '1,1')
 	})
 
 	it('starts a new table after an empty line', () => {
@@ -169,18 +170,18 @@ describe.skip('parseMarkdown: Tables', () => {
 
 		const result = parseMarkdown(markdown)
 
-		expect(result.content).toHaveLength(3)
+		expect(result.content).toHaveLength(2)
 
 		assume(result.content[0]).toHaveProperty('type', 'Table')
 
-		expect(result.content[2]).toHaveProperty('type', 'Table')
-		const table = result.content[2] as Table
+		expect(result.content[1]).toHaveProperty('type', 'Table')
+		const table = result.content[1] as unknown as Table
 
-		expect(table.headings[0].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[0].content[0]).toHaveProperty('content', 'ghi')
+		expect(table.headers?.columns[0].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[0].content[0]).toHaveProperty('textContent', ' ghi ')
 
-		expect(table.headings[1].content[0]).toHaveProperty('type', 'Text')
-		expect(table.headings[1].content[0]).toHaveProperty('content', 'jkl')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('type', 'Text')
+		expect(table.headers?.columns[1].content[0]).toHaveProperty('textContent', ' jkl ')
 	})
 
 	it('does not parse content as table when delimiters are missing', () => {
@@ -191,31 +192,18 @@ describe.skip('parseMarkdown: Tables', () => {
 		expect(result.content[0]).toHaveProperty('type', 'Paragraph')
 	})
 
-	it('creates number of columns from delimiter row in each row', () => {
-		const markdown = '|foo|bar|baz\n|-|-|\n| abc |\n|def| ghi | jkl |\n'
-
-		const result = parseMarkdown(markdown)
-
-		assume(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
-
-		expect(table.rows[0].columns).toHaveLength(2)
-		expect(table.rows[1].columns).toHaveLength(2)
-		expect(table.headings).toHaveLength(2)
-	})
-
 	it('parses delimiters with correct alignment', () => {
 		const markdown = '|-|:-|:-:|-:|\n| abc |\n'
 
 		const result = parseMarkdown(markdown)
 
 		assume(result.content[0]).toHaveProperty('type', 'Table')
-		const table = result.content[0] as Table
+		const table = result.content[0] as unknown as Table
 
-		expect(table.columns).toHaveLength(4)
-		expect(table.columns?.[0]).toHaveProperty('align', 'left')
-		expect(table.columns?.[1]).toHaveProperty('align', 'left')
-		expect(table.columns?.[2]).toHaveProperty('align', 'center')
-		expect(table.columns?.[3]).toHaveProperty('align', 'right')
+		expect(table.delimiters.columns).toHaveLength(4)
+		expect(table.delimiters.columns?.[0]).toHaveProperty('alignment', 'left')
+		expect(table.delimiters.columns?.[1]).toHaveProperty('alignment', 'left')
+		expect(table.delimiters.columns?.[2]).toHaveProperty('alignment', 'center')
+		expect(table.delimiters.columns?.[3]).toHaveProperty('alignment', 'right')
 	})
 })
